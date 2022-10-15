945960270ce7a629f8e0409dda2bb5c550bf5703
safazaidmalik <75358020+safazaidmalik@users.noreply.github.com>
2022-10-15 22:39:18 +0500

    Update Adjectives_Extraction.py

--- Adjectives_Extraction.py
+++ Adjectives_Extraction.py
@@ -3,8 +3,6 @@
 If adjective following article or number, it describes the next appearing noun/pronoun
 e.g. That is a strong dog.
 
-
-
 If adjective is following verb to be, it describes last appearing noun/pronoun. 
 e.g. The dog is strong.
 
@@ -13,23 +11,10 @@ e.g. The dog is strong.
 import spacy
 import numpy as np
 
-verbs_to_be = ['are', 'am', 'is', 'was', 'were', 'been', 'being']
-
-
 def set_pos(chunk):
-    
-
     intra_obj_relation = {}
     sp = spacy.load('en_core_web_sm')
     sen = sp(chunk)
-    adj_pos = -1
-    article_pos = -1
-    obj_pos = -1
-    verb_to_be_pos = -1
-    article = ''
-    adj = ''
-    obj = ''
-    verb_to_be = ''
 
     lis_nouns = []
     lis_propn = []
@@ -61,16 +46,9 @@ def set_pos(chunk):
         elif sen[i].pos_ == 'CCONJ':
             lis_con.append(i)
 
-    # print('lis_nouns = ', lis_nouns)            
-    # print('lis_verbs_to_be = ', lis_verbs_to_be)
-    
-    #Github Desktop
-
     for noun in lis_nouns:
         intra_obj_relation[sen[noun]] = {'adj':[],'article':[]}
         
-
-# Start adj from left to right
 # Can have one or more than one adjectives per noun
     for adj_index in lis_adj:
         adj_list_adjuster_back = 0  # count of how many adjectives and/or conjunctions are between the noun and the current adjective
@@ -108,7 +86,5 @@ def set_pos(chunk):
     return intra_obj_relation
 
 
-
 text = input('Input query here :')
-
 print(set_pos(text))
