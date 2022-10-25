99c0d4e65c28261471af0185c3f8dd80eee58051
safazaidmalik <75358020+safazaidmalik@users.noreply.github.com>
2022-10-25 19:27:38 +0500

    Dependency parsing using StanfordDependencyParser
    This program generates a dependency tree given a text input. It also creaes an equivalent graph plot from the tree. Since StanfordDependencyParser will soon be deprecated, CoreNLPParser may be used as an alternative library for performing the task.

---
+++ dependency_parsing.py
@@ -0,0 +1,40 @@
+from nltk.parse.stanford import StanfordDependencyParser
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Your path to CoreNLP jar
+jar_path = '/home/szm/Downloads/stanford-corenlp-4.5.1/stanford-corenlp-4.5.1.jar'
+
+# Path to CoreNLP model jar
+models_jar_path = '/home/szm/Downloads/stanford-corenlp-4.5.1/stanford-corenlp-4.5.1-models.jar'
+
+# sentence = 'Deemed universities charge huge fees'
+sentence = "The tree has yellow and green leaves."
+
+# Initialize StanfordDependency Parser from the path
+parser = StanfordDependencyParser(path_to_jar = jar_path, path_to_models_jar = models_jar_path)
+
+# Parse the sentence
+result = parser.raw_parse(sentence)
+dependency = result.__next__()
+
+print ("{:<15} | {:<10} | {:<10} | {:<15} | {:<10}".format('Head', 'Head POS','Relation','Dependent', 'Dependent POS'))
+print ("-" * 75)
+  
+# Use dependency.triples() to extract the dependency triples in the form
+# ((head word, head POS), relation, (dependent word, dependent POS))  
+for dep in list(dependency.triples()):
+  print ("{:<15} | {:<10} | {:<10} | {:<15} | {:<10}"
+         .format(str(dep[0][0]),str(dep[0][1]), str(dep[1]), str(dep[2][0]),str(dep[2][1])))
+
+# Using reverse() to reverse the direction of edges as nx_graph() returns inverted edges
+G = dependency.nx_graph().reverse()
+print(G)
+# nx_graph() returns numeric node labels starting from 1
+# Create a dictionary to map numeric nodes and words in the sentence
+words = sentence.split(" ")
+labels = {index + 1: words[index] for index in range(len(words))}
+
+print("Going to draw G")
+nx.draw(G, with_labels=True, labels=labels, node_size=2500, node_color='#B5EAD7', font_size=10)
+plt.show()
