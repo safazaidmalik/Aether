411aa98cad6a3ed546e165b39e66bd223c96115e
safazaidmalik <75358020+safazaidmalik@users.noreply.github.com>
2022-10-26 23:20:30 +0500

    Dependency parsing graph generation
    Graph is being generated for multiple subject-object-relation triplets, and for repeating relations but different subject-object pairs.
    StanfordDependencyParser, networkx and matplotlib are being used.

--- dependency_parsing/dependency_parsing_noun_based.py
+++ dependency_parsing/dependency_parsing_noun_based.py
@@ -2,10 +2,15 @@ from nltk.parse.stanford import StanfordDependencyParser
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Change paths according to your own paths
+# Add your own paths here
 jar_path = '/home/szm/Downloads/stanford-corenlp-4.5.1/stanford-corenlp-4.5.1.jar'
 models_jar_path = '/home/szm/Downloads/stanford-corenlp-4.5.1/stanford-corenlp-4.5.1-models.jar'
-sentence = "The girl has brown eyes and red hair."
+# sentence = "The horse wears a saddle. Two girls own the saddle. They wear brown dresses."
+# without coreference resolution - 'A girl' and 'Another girl' are shown as the same entity
+
+
+sentence = "The horse wears a saddle. A girl owns the saddle. The girl wears a brown dress."
+print("Input sentence = ", sentence)
 
 # intitialize parser
 parser = StanfordDependencyParser(
@@ -31,45 +36,50 @@ G = nx.MultiDiGraph()
 #  ie dep[1][0]=='NN' or 'NNS' (entity), and one of them is a subject (ie dep[1] == 'nsubj')
 #  while the other is an object (ie dep[1] == 'obj'),
 #  add nodes to the graph and connect them with label = dep[0][0]
-counter = 1
-nodes_list = []
-relation_text = []
+nouns_list = ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'PRP$']
+verbs_list = ['VB', 'VBG', 'VBN', 'VBP', 'VBZ', 'VBD']
+relations_list = {'subject': ['nsubj', 'det:poss', 'nmod', 'nmod:poss', 'nmod:tmod', 'nsubj:move', 'nummod',
+                              'nummod:gov'], 'object': ['obj', 'obl', 'iobj', 'dobj', 'obl:agent', 'obl:arg', 'obl:lmod', 'obl:tmod']}
+triples = {}
+relations = []
 for dep in list(dependency.triples()):
-    if dep[0][1] == 'VBZ':
-        # store label verb in labels_list
-        relation_text.append(dep[0][0])
+    if dep[0][1] in verbs_list:
+        relations.append(dep[0][0])
 
-# match dep[0][0] with labels_list
-node_1 = ''
-node_2 = ''
+for rel in relations:
+    # append relevant subject,object tuples to triples[rel] later
+    triples[rel] = []
+relations = list(set(triples))
+print("Relations = ", relations)
+tmp_sbj = ''
 sbj_found = 0
 obj_found = 0
-
-nouns_list = ['NN', 'NNS', 'NNP', 'NNPS']
-
+tmp_obj = ''
 for dep in list(dependency.triples()):
-    if dep[0][1] == 'VBZ' and dep[0][0] in relation_text:
-        print("Dependent word with Common Head of ",
-              dep[0][0], " is ", dep[2][0])
-        if dep[2][1] in nouns_list:
-            print("Dependent of common Head 'has' = ", dep[2][0])
-            if dep[1] == 'nsubj':
-                sbj_found = 1
-                node_1 = dep[2][0]
-                # print("Found subject!")
-            elif dep[1] == 'obj':
-                obj_found = 1
-                node_2 = dep[2][0]
-                # print("Found object!")
+    if dep[0][1] in verbs_list:
+        if dep[0][0] in relations:
+            if dep[2][1] in nouns_list:
+                if dep[1] in relations_list['subject']:
+                    tmp_sbj = dep[2][0]
+                    sbj_found = 1
+                elif dep[1] in relations_list['object']:
+                    tmp_obj = dep[2][0]
+                    obj_found = 1
+                    # sbj_found = 0
+    if sbj_found == 1 and obj_found == 1:
+        triples[dep[0][0]].append((tmp_sbj, tmp_obj))
+        sbj_found = 0
+        obj_found = 0
+print(triples)
 
-nodes = [node_1, node_2]
-labels = {(node_1, node_2): relation_text[0]}
-print("Labels = ", labels)
-G.add_edge(node_1, node_2, label=relation_text[0])
+for rel in relations:
+    for edges in triples[rel]:
+        print("edges = ", edges)
+        G.add_edge(edges[0], edges[1], label=rel)
+        print("Added to graph: ", edges[0], edges[1], rel)
 pos = nx.spring_layout(G)
 plt.figure()
 
 nx.draw(G, pos, with_labels=True, node_size=2500,
-        node_color='#B5EAD7', font_size=10)
-
+        node_color='#B5EAD7', font_size=5)
 plt.show()
